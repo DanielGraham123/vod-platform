bef9c1579d9c82e9dd0879e7ca46c2de1565a2dc
DanielGraham123 <dg.danielboaz@yahoo.com>
2021-10-15 11:02:03 +0100

    modified index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,23 @@
 <template>
   <div>
     <Loader />
-    
+
     <div class="main-content">
       <Landing />
     </div>
-    <!-- <div class="main-content">
-      <favourite />
-      <upcomming />
-       <top /> -->
-    <!-- <suggestion /> -->
-    <!-- <detail /> -->
-    <!-- <trending /> -->
-    <!-- <thriller /> -->
-    <!-- </div> -->
   </div>
 </template>
 
 <script>
-import { core } from "../assets/app/app";
-import Home from "../components/frontend/home-page/Home";
 import Landing from "../components/frontend/landing-page/Landing";
-import favourite from "../components/frontend/home-page/favourite";
-import upcomming from "../components/frontend/home-page/upcomming";
-import top from "../components/frontend/home-page/top-ten";
-import suggestion from "../components/frontend/home-page/suggestion";
-import detail from "../components/frontend/home-page/detail";
-import trending from "../components/frontend/home-page/trending";
-import thriller from "../components/frontend/home-page/thriller";
-import Loader from '../components/core/loader/Loader'
-
+import Loader from "../components/core/loader/Loader";
 
 export default {
   layout: "FrontendLayout",
   components: {
-    Home,
     Landing,
-    favourite,
-    upcomming,
-    top,
-    suggestion,
-    detail,
-    trending,
-    thriller,
     Loader,
   },
-  mounted() {
-    setTimeout(function () {
-      core.index();
-    }, 1000);
-  },
+  mounted() {},
 };
 </script>
